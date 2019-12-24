cb46b3401f326d02de3e2d261c9361039442db1b
yangzhilong <yangzhilong@lanjinrong.com>
2019-12-24 17:58:52 +0800

    12-24

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -5,7 +5,7 @@
 
 import React, { useState, useEffect } from 'react';
 const Test = () => {
-  const [a] = useState('123456');
+  const [a] = useState('123456789');
   useEffect(() => {
       console.log('start')
       return () => {
